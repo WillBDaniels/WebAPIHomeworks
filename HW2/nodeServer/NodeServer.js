6e5812c0a3a9a0c6874f64d2aeff0287c9df0378
William Daniels <willbdaniels@gmail.com>
2015-03-02 18:08:33 -0700

    named the file something legit. Also caught up the build with the active apigee one, need a better way to sync those...

--- HW2/nodeServer/NodeServer.js
+++ HW2/nodeServer/NodeServer.js
@@ -1,45 +1,57 @@
+/*
+* William Daniels
+* Web API HW2 node.js server
+* This server makes a web endpoint that can respons to a variety of different types of requests, and respond accordingly
+* It can currently parse up queries, request type, as well as a general echo of headers sent to the server. 
+*/
 var sys = require("sys");
 var my_http = require("http");
 var url = require("url");
 my_http.createServer(function(request, response){
+	//Chrome likes to send two requests every time, one looking for the favicon. I don't want to waste cycles on a favicon, so I check for
+	//this specific case. 
     if(request.url === '/favicon.ico') {
         response.end();
     }else{
 		var responseText = "";
 		var method = request.method;
 		var urlStuff = url.parse(request.url, true);
+		var deniedRequest = "ERROR: This server location doesn't accept the request you attempted, please try again.";
 		switch(method){
 			case "GET":
 				if (urlStuff.pathname == '/get'){
 					responseText = "GET request accepted, information about request: ";
 					responseText += "\n" + buildResponeToClient(request);
 				}else{
-					responseText = "This server URL doesn't accept GET requests, I'm sorry.";
+					responseText = deniedRequest;
 				}
 				break;
 			case "POST":
 				if (urlStuff.pathname == '/post'){
 			 		responseText = "I suppose POST requests are ok too...accepted";
+                  	responseText += "\n" + buildResponeToClient(request);
 			 	}else{
-			 		responseText = "This server location doesn't accept POST requests, I'm sorry.";
+			 		responseText = deniedRequest;
 			 	}
 			 	break;
 		 	case "PUT":
 		 		if (urlStuff.pathname == '/put'){
 		 			responseText = "Alright, seriously, who even uses PUT anymore???... Fine. Accepted";
+                  	responseText += "\n" + buildResponeToClient(request);
 		 		}else{
-		 			responseText = "This server location doesn't accept PUT requests, I'm sorry.";
+		 			responseText = deniedRequest;
 		 		}
 		 		break;
 		 	case "DELETE":
 		 		if (urlStuff.pathname == '/delete'){
 		 			responseText = "Ok. I don't know why I'm accepting DELETE, but whatever, accepted.";
+                  	responseText += "\n" + buildResponeToClient(request);
 		 		}else{
-		 			responseText = "This server location doesn't accept DELETE requests, I'm sorry.";
+		 			responseText = deniedRequest;
 		 		}
 		 		break;
 		 	default:
-		 		responseText = "request rejected. I'm pretty lenient, and even I don't know what to do with this. Rejected.";
+		 		responseText = deniedRequest;
 		}
 		//response.writeHeader(200, {"Content-Type": "text/plain"});
 		response.write(responseText);
